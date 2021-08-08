433b46db5de96a46e35573473769bb9b1c0e9587
Guarlic <85231330+Guarlic@users.noreply.github.com>
2021-08-08 20:59:28 +0900

    First

---
+++ sort.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <algorithm>
+#include <array>
+
+using namespace std;
+
+int main() {
+    array<int, 6> ar = { 4, 3, 6, 5, 1, 2 };
+   
+    sort(ar.begin(), ar.end());
+    
+    for (int i = 0; i < 6; i++) {
+        printf("%d ", ar[i]);
+    }
+    
+    printf("\n");
+    
+    return 0;
+}
+
