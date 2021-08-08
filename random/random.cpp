f8cea2a4090eb009bd30539af01ebfbce31e52f4
Guarlic <85231330+Guarlic@users.noreply.github.com>
2021-08-09 08:56:56 +0900

    Add Random

---
+++ random/random.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <array>
+#include <algorithm>
+
+int main() {
+    srand(time(0));
+    
+    std::array<int, 10> ar;
+    
+    for (int i = 0; i < 10; i++) {
+        ar[i] = rand() % 10 + 1;
+    }
+   
+    sort(ar.begin(), ar.end());
+    
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", ar[i]); 
+    }
+    
+    printf("\n");
+    
+    return 0;
+}
