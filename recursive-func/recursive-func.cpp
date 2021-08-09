d0e023a714a6008c041bd9e23009da12bd9c2bc4
Guarlic <85231330+Guarlic@users.noreply.github.com>
2021-08-09 21:19:41 +0900

    add recursive-func

---
+++ recursive-func/recursive-func.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+int factorial(int num) {
+    if (num <= 1) return num;
+
+    return factorial(num - 1) * num;
+}
+
+int main() {
+    int num;
+
+    printf("Inser num: ");
+    scanf("%d", &num);
+
+    printf("%d! = %d\n", num, factorial(num));
+
+    return 0;
+}
