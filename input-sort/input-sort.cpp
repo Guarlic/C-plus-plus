b7f33c5efd2eb7e0aa245a1a618e67abc1bce18c
Guarlic <85231330+Guarlic@users.noreply.github.com>
2021-08-09 14:11:12 +0900

    Add input-sort

---
+++ input-sort/input-sort.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <array>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    array<int, 6> ar;
+
+    for (int i = 0; i < 6; i++) {
+        scanf("%d", &ar[i]);
+    }
+
+    sort(ar.begin(), ar.end());
+
+    for (int i = 0; i < 6; i++) {
+        printf("%d ", ar[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
