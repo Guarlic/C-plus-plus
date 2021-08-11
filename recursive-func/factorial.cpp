62b30aeb5371c68a8fc3238a037577116906b5c7
hyunu <hyunujung0419@gmail.com>
2021-08-11 19:19:37 +0900

    Update recursive-func2

--- recursive-func/factorial.cpp
+++ recursive-func/factorial.cpp
@@ -2,17 +2,17 @@
 
 int factorial(int num) {
     if (num <= 1) return num;
-
+    
     return factorial(num - 1) * num;
 }
 
 int main() {
     int num;
-
+    
     printf("Inser num: ");
     scanf("%d", &num);
-
+    
     printf("%d! = %d\n", num, factorial(num));
-
+    
     return 0;
 }
