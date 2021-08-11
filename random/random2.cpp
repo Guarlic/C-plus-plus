e13532041c911e97cd9a0da08b2d155352926c1f
hyunu <hyunujung0419@gmail.com>
2021-08-11 20:11:28 +0900

    Add random2

---
+++ random/random2.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <array>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    srand(time(0));
+    
+    int num;
+    scanf("%d", &num);
+    int* ar = new int[num];
+    
+    for (int i = 0; i < num; i++) {
+        ar[i] = rand() % 10 + 1;
+    }
+    
+    sort(ar, ar + num);
+    
+    for (int i = 0; i < num; i++) {
+        printf("%d ", ar[i]);
+    }
+    
+    printf("\n");
+    
+    return 0;
+}
