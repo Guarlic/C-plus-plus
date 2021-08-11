8d008715af6e02f3bc27b7568aa6a69befcb36bc
hyunu <hyunujung0419@gmail.com>
2021-08-11 17:20:37 +0900

    Update input-sort.cpp

--- sort/input-sort/input-sort.cpp
+++ sort/input-sort/input-sort.cpp
@@ -6,18 +6,18 @@ using namespace std;
 
 int main() {
     array<int, 6> ar;
-
+    
     for (int i = 0; i < 6; i++) {
         scanf("%d", &ar[i]);
     }
-
+    
     sort(ar.begin(), ar.end());
-
+    
     for (int i = 0; i < 6; i++) {
         printf("%d ", ar[i]);
     }
-
+    
     printf("\n");
-
+   
     return 0;
 }
