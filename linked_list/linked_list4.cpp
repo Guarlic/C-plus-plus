846a656e74bca1c0dead7fc8164bf30b63e273a4
hyunu <hyunujung0419@gmail.com>
2021-08-16 16:02:58 +0900

    Update linked_list4

--- linked_list/linked_list4.cpp
+++ linked_list/linked_list4.cpp
@@ -1,6 +1,5 @@
 #include <cstdio>
 
-// 구조체 정의
 struct node {
     node* next;
     node* prev;
@@ -8,15 +7,12 @@ struct node {
 };
 
 int main() {
-    // 구조체들 선언
     node* head = new node;
     node* node1 = new node;
     node* node2 = new node;
     node* node3 = new node;
     node* tail = new node;
 
-    // data, prev, next 에 값 대입
-    head->prev = NULL;
     head->next = node1;
     node1->data = 10;
     node1->prev = head;
@@ -33,12 +29,10 @@ int main() {
 
     scanf("%d%d", &input, &input2);
 
-    // input - 1 만큼 curr을 next로 이동
     for (int i = 0; i < input - 1; i++) {
         curr = curr->next;
     }
 
-    // node3 추가 작업
     node3->data = input2;
     node3->next = curr->next;
     curr->next->prev = node3;
@@ -47,13 +41,11 @@ int main() {
     
     curr = head->next;
 
-    // data들 출력
     while (curr->next != NULL) {
         printf("%d\n", curr->data);
         curr = curr->next;
     }
 
-    // 공간 해제 및 프로그램 종료 (0 반환)
     delete head;
     delete node1;
     delete node2;
