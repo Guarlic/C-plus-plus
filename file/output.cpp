75deba8fa3c29224b7317aeb880e2d2980787346
hyunu <hyunujung0419@gmail.com>
2021-08-22 16:55:00 +0900

    Add output

---
+++ file/output.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+
+int main() {
+    FILE* fp;
+    char input;
+    char string[100];
+
+    scanf("%s", string);
+
+    fp = fopen(string, "r");
+
+    if (fp == NULL) {
+        printf("Error; 존재하지 않는 파일입니다: %s\n", string);
+        return 0;
+    }
+
+    while (fscanf(fp, "%c", &input) != EOF) {
+        printf("%c", input);
+    }
+
+    fclose(fp);
+    return 0;
+}
