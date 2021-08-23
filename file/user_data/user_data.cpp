934a8920e1fffc70dd90d3118b2c040ac9c12258
hyunu <hyunujung0419@gmail.com>
2021-08-23 09:36:16 +0900

    Update user_data

--- file/user_data/user_data.cpp
+++ file/user_data/user_data.cpp
@@ -13,23 +13,33 @@ void Sputnget(const char* Text, char* Var) {
 
 int main() {
     FILE* fp;
-    User user[3];
+    int cnt;
     char temp;
-    fp = fopen("info.txt", "wr");
+    fp = fopen("info.txt", "r");
 
     printf("* 이전 데이터 -----\n");
     while (fscanf(fp, "%c", &temp) != EOF) {
         printf("%c", temp);
     }
+    
+    fclose(fp);
+    fp = fopen("info.txt", "w");
 
     printf("\n* 데이터 입력 -----\n");
-    for (int i = 0; i < 3; i++) {
+    printf("Num of Users: ");
+    scanf("%d", &cnt);
+
+    User* user = new User[cnt];
+
+    for (int i = 0; i < cnt; i++) {
         Sputnget("ID: ", user[i].id);
         Sputnget("Password: ", user[i].pswd);
 
         fprintf(fp, "%s %s\n", user[i].id, user[i].pswd);
     }
 
+    delete[] user;
     fclose(fp);
+
     return 0;
 }
